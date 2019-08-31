40028ecc6d651d2d0c12fd95f4f06bbfa169c1b8
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-08-31 19:29:14 -0400

    Added typescript to header to see dynamic reloading of content in browser.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const App: React.FC = () => {
-    return <h1>My React App!</h1>;
+    return <h1>My React and Typescript App!</h1>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
